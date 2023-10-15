8637e9aac40cb24a59f4ed17a2ce39cc718e2dd9
Syed Irfan Faraz <irfanfaraaz03@gmail.com>
2023-10-15 12:50:08 +0530

    feat:theme

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,13 +1,17 @@
+import Logo from "@/components/Logo";
+import ThemeSwitcher from "@/components/ThemeSwitcher";
 import { UserButton } from "@clerk/nextjs";
 import React, { ReactNode } from "react";
 
 const Layout = ({ children }: { children: ReactNode }) => {
     return (
         <div className="flex flex-col min-h-screen min-w-full bg-background max-h-screen">
-            <nav>
+            <nav className="flex justify-between items-center border-b border-border h-[60px] px-4 py-2">
                 <Logo />
-                <ThemeSwitcher />
-                <UserButton afterSignOutUrl="/sign-in" />
+                <div className="flex gap-4 items-center">
+                    <ThemeSwitcher />
+                    <UserButton afterSignOutUrl="/sign-in" />
+                </div>
             </nav>
             <main className="flex w-full flex-grow">{children}</main>
         </div>

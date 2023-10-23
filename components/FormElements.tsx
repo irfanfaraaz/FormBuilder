838344112def6107d9debe50fe058aa5e179d344
Syed Irfan Faraz <irfanfaraaz03@gmail.com>
2023-10-23 14:43:05 +0530

    feat:form submit and validations

--- components/FormElements.tsx
+++ components/FormElements.tsx
@@ -2,6 +2,8 @@ import { TextFieldFormElement } from "./fields/TextField";
 
 export type ElementsType = "TextField";
 
+export type SubmitFunction = (key: string, value: string) => void;
+
 export type FormElement = {
     type: ElementsType;
 
@@ -16,10 +18,17 @@ export type FormElement = {
     }>;
     formComponent: React.FC<{
         elementInstance: FormElementInstance;
+        submitValue?: (key: string, value: string) => void;
+        isInvalid?: boolean;
+        defaultValue?: string;
     }>;
     propertiesComponent: React.FC<{
         elementInstance: FormElementInstance;
     }>;
+    validate: (
+        formElement: FormElementInstance,
+        currentValue: string
+    ) => boolean;
 };
 
 export type FormElementInstance = {
